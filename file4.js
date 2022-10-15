b0b0e72c79da8ec5c6cccd1cddd08bf93afc4ceb
Himansh Mishra <88498643+Himuu14@users.noreply.github.com>
2022-10-15 12:22:09 +0530

    Update file4.js

--- file4.js
+++ file4.js
@@ -5,10 +5,27 @@
 // console.log(fruits);
 // fruits.pop();
 // console.log(fruits);
-let fruits = ["mango","Banana", "Watermellon"];
-var x = fruits.length;
-let i=0;
- for(i;i<x;i++){
-     console.log(fruits[i]);
+
+
+// let fruits = ["mango","Banana", "Watermellon"];
+// var x = fruits.length;
+// let i=0;
+//  for(i;i<x;i++){
+//      console.log(fruits[i]);
+//  }
+//  console.log(x);
+
+//Number gussing game;
+let number =6;
+let guess=3;
+while(guess){
+ let inputNumber = prompt("You have to guess the number in 3 chance number is in between 1 to 100");
+ if(intputNumber =number){
+  console.log("Wonderfull you have got it ");
+  break;
  }
- console.log(x);
+ else{
+  console.log("No try again");
+  guess--;
+ }
+}
