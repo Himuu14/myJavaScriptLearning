d3a854b4cb2eec0e6a91db82a7d4d1758c2243a6
Himansh Mishra <88498643+Himuu14@users.noreply.github.com>
2022-10-15 12:16:01 +0530

    Update file3.js

--- file3.js
+++ file3.js
@@ -7,10 +7,18 @@
 // }
 // console.log(total);
 
-let i =0;
+// let i =0;
 
-let total=0;
-for(i;i<=10;i++){
-    total +=i;
+// let total=0;
+// for(i;i<=10;i++){
+//     total +=i;
+// }
+// console.log(total);
+
+let noc =8; //Number of column
+
+for(int i=1;i<=noc;i++){
+    for(int j=1;j<i;j++){
+        console.log(j+ " ");
+    }console.log();
 }
-console.log(total);
